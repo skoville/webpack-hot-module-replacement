79471bb9f0323e6dff8b9fc64772847630afc3ba
AlexLeung <alex.l.leung@gmail.com>
2019-07-22 22:33:01 -0700

    added onto webpack configuration for integration test. Moving towards ability to load a custom node bundle

--- packages/@skoville-integration-testing/default-test/source/isomorphic-bundle-test.ts
+++ packages/@skoville-integration-testing/default-test/source/isomorphic-bundle-test.ts
@@ -1,5 +1,10 @@
+import * as path from 'path';
 import { DefaultNodeServer, SkovilleWebpackPlugin } from '@skoville/webpack-hmr-node-server-default';
 import * as webpack from 'webpack';
+import { fsAsync } from '@isomorphic-typescript/fs-async-nodejs';
+
+const projectPackageJSON = require('../package.json');
+projectPackageJSON;
 
 const skovillePlugin = new SkovilleWebpackPlugin({
     client: {
@@ -10,17 +15,48 @@ const skovillePlugin = new SkovilleWebpackPlugin({
     server: new DefaultNodeServer(true, 8080)
 });
 
+const PROJECT_PATH = path.resolve(__dirname, "../");
+const BUNDLE_SOURCE_PATH = path.resolve(PROJECT_PATH, "source-bundle");
+const BUNDLE_OUT_PATH = path.resolve(__dirname, "./bundles"); // This is relative to the dist folder after compilation.
+
 const configs: webpack.Configuration[] = [
     {
+        mode: 'development',
+        entry: path.resolve(BUNDLE_SOURCE_PATH, "./node/server.ts"),
         plugins: [
             new webpack.HotModuleReplacementPlugin(),
             skovillePlugin
-        ]
+        ],
+        target: 'node',
+        output: {
+            path: BUNDLE_OUT_PATH,
+            filename: 'server.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: {
+                        loader: 'ts-loader',
+                        options: {
+                            //configFile: path.resolve(PROJECT_PATH, "./tsconfig.json")
+                        }
+                    }
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.ts']
+        }
     },
+    // TODO: add entry for web bundle.
 ];
 
-webpack(configs).watch({}, async (error: Error, stats: webpack.Stats) => {
-    error;
-    stats;
-    console.log("run output");
-});
+fsAsync.comprehensiveDeleteAsync(BUNDLE_OUT_PATH)
+    .then(() => {
+        webpack(configs).watch({}, async (error: Error, stats: webpack.Stats) => {
+            error;
+            stats;
+            console.log("run output");
+        });
+    });
